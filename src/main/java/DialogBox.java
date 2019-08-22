797f5fc829d4fec27dd31153fe54d3299c7eff32
lmtaek <ltaekman@gmail.com>
2019-08-23 00:05:52 +0800

    added padding between dialogboxes, and colors depending on which user is speaking.

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,22 +1,54 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 
 public class DialogBox extends HBox {
 
     private Label text;
     private ImageView displayPicture;
 
+
     public DialogBox(Label l, ImageView iv) {
         text = l;
         displayPicture = iv;
 
         text.setWrapText(true);
+        text.setStyle("-fx-padding: 0 10 0 10");
         displayPicture.setFitWidth(100.0);
         displayPicture.setFitHeight(100.0);
 
         this.setAlignment(Pos.TOP_RIGHT);
         this.getChildren().addAll(text, displayPicture);
+    }
+
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(Label l, ImageView iv) {
+        var ub = new DialogBox(l, iv);
+        ub.setPadding(new Insets(10, 0, 10, 0));
+        ub.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
+        return ub;
+    }
+
+    public static DialogBox getDukeDialog(Label l, ImageView iv) {
+        var db = new DialogBox(l, iv);
+        db.flip();
+        db.setPadding(new Insets(0, 0, 0, 0));
+        db.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+        return db;
     }
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,15 @@
 import javafx.application.Application;
-import javafx.scene.control.Label;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.layout.Region;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class Duke extends Application {
 
@@ -101,8 +101,9 @@ public class Duke extends Application {
         Label userText = new Label(userInput.getText());
         Label dukeText = new Label(getResponse(userInput.getText()));
         dialogContainer.getChildren().addAll(
-                new DialogBox(userText, new ImageView(user)),
-                new DialogBox(dukeText, new ImageView(duke))
+                DialogBox.getUserDialog(userText, new ImageView(user)),
+                //ADD PADDING HERE SOMEHOW. dialogContainer.setPadding(new Insets(20, 0, 0, 0));
+                DialogBox.getDukeDialog(dukeText, new ImageView(duke))
         );
         userInput.clear();
     }
