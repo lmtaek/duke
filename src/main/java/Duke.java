92284e10d5c13df7c9061726ac11f9cd93cae3f6
lmtaek <ltaekman@gmail.com>
2019-08-27 20:49:43 +0800

    back-tracked and removed GUI, gradle, etc. in order to see if scanner would start working again. success.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static String dukeGreeting = "Hello, I'm Duke.\nWhat can I do to help you?";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +11,28 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(dukeGreeting + "\r\n");
+
+        // The user will enter commands from this point onwards.
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNextLine()) {
+            String userInput = input.nextLine();
+
+            if (userInput.toLowerCase().equals("bye")) {
+                closeApplication();
+            }
+
+            System.out.println("\t" + userInput);
+            continue;
+        }
+
     }
+
+    static void closeApplication() {
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.exit(0);
+    }
+
 }
